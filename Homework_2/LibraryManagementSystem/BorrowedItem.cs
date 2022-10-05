78528c80fcedc8e80860c08527dc0bb4c89239e1
yuanhao <t109590041@ntut.org.tw>
2022-10-05 23:17:59 +0800

    [HomeWork2] feat: model, 更新 BorrowedItem 物件

--- Homework_2/LibraryManagementSystem/BorrowedItem.cs
+++ Homework_2/LibraryManagementSystem/BorrowedItem.cs
@@ -9,6 +9,7 @@ namespace LibraryManagementSystem
     public class BorrowedItem
     {
         private DateTime _date;
+        private Book _book;
 
         #region Constructor
         public BorrowedItem()
@@ -27,6 +28,18 @@ namespace LibraryManagementSystem
             set
             {
                 _date = value;
+            }
+        }
+
+        public Book Book 
+        {
+            get
+            {
+                return _book;
+            }
+            set
+            {
+                _book = value;
             }
         }
         #endregion
