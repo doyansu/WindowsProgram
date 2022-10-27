6d80b0e281e40f03f94a8c07606f0aeb05188b53
doyansu <fyuanhao91@gmail.com>
2022-10-27 14:54:57 +0800

    [UnitTestLab] 單元測試練習

---
+++ UnitTestLab/Bank/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class Account
+    {
+        const string ERROR_MSG = "Sorry, you don't have so much money!!";
+        private List<double> _records;
+        public Account(uint initAmount)
+        {
+            _records = new List<double>();
+            Deposit(initAmount);
+        }
+        // Note: we make Balance private to demonstrate the testing of private members.
+        private double Balance
+        {
+            get
+            {
+                double balance = 0;
+                foreach (double record in _records)
+                {
+                    balance += record;
+                }
+                return balance;
+            }
+        }
+        public void Deposit(uint amount)
+        {
+            _records.Add(Decimal.ToDouble(amount));
+        }
+        public void Withdraw(uint amount)
+        {
+            if (amount > Balance)
+                Console.WriteLine(ERROR_MSG);
+            else
+                _records.Add(Decimal.ToDouble(-amount));
+        }
+    }
+}
---
+++ UnitTestLab/BankTests/AccountTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank.Tests
+{
+    [TestClass()]
+    public class AccountTests
+    {
+        const uint INIT_AMOUNT = 100;
+        const uint DEPOSITED = 200;
+        const uint BIG_WITHDRAW = 300;
+        PrivateObject _accountPrivate;
+        Account _account;
+        [TestInitialize()]
+        public void Initialize()
+        {
+            _account = new Account(INIT_AMOUNT);
+            _accountPrivate = new PrivateObject(_account);
+        }
+        [TestMethod()]
+        public void DepositTest()
+        {
+            Assert.AreEqual((double)INIT_AMOUNT, _accountPrivate.GetFieldOrProperty("Balance"));
+            _account.Deposit(DEPOSITED);
+            Assert.AreEqual((double)(INIT_AMOUNT + DEPOSITED),
+            _accountPrivate.GetFieldOrProperty("Balance"));
+        }
+        [TestMethod()]
+        public void WithdrawTest()
+        {
+            Assert.AreEqual((double)INIT_AMOUNT,  _accountPrivate.GetFieldOrProperty("Balance")); 
+            _account.Withdraw(BIG_WITHDRAW);
+            Assert.AreEqual((double)INIT_AMOUNT, _accountPrivate.GetFieldOrProperty("Balance"));
+            _account.Deposit(DEPOSITED);
+            Assert.AreEqual((double)(INIT_AMOUNT + DEPOSITED), _accountPrivate.GetFieldOrProperty("Balance"));
+            _account.Withdraw(BIG_WITHDRAW);
+            double expected = (double)(INIT_AMOUNT + DEPOSITED) - BIG_WITHDRAW;
+            Assert.AreEqual(expected, _accountPrivate.GetFieldOrProperty("Balance"));
+        }
+        [TestMethod()]
+        public void BalanceTest()
+        {
+            Assert.AreEqual((double)INIT_AMOUNT,
+            _accountPrivate.GetFieldOrProperty("Balance"));
+        }
+    }
+}
