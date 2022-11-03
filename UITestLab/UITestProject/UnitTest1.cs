4b2533bccb4cb83fe706d413a625f988c42e68d2
doyansu <fyuanhao91@gmail.com>
2022-11-03 15:01:27 +0800

    [UITestLab] UI測試練習

---
+++ UITestLab/UITestProject/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UITestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Robot _robot;
+        private const string APP_NAME =
+       "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
+        private const string CALCULATOR_TITLE = "小算盤";
+        private const string EXPECTED_VALUE = "顯示是 444";
+        private const string RESULT_CONTROL_NAME = "CalculatorResults";
+        /// <summary>
+        /// Launches the Calculator
+        /// </summary>
+        [TestInitialize()]
+        public void Initialize()
+        {
+            _robot = new Robot(APP_NAME, CALCULATOR_TITLE);
+        }
+        /// <summary>
+        /// Closes the launched program
+        /// </summary>
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _robot.CleanUp();
+        }
+
+        /// <summary>
+        /// Tests that the result of 123 + 321 should be 444
+        /// </summary>
+        [TestMethod]
+        public void TestAdd()
+        {
+            RunScriptAdd();
+            _robot.AssertText(RESULT_CONTROL_NAME, EXPECTED_VALUE);
+        }
+
+        /// <summary>
+        /// Runs the script: 123 + 321 =
+        /// </summary>
+        private void RunScriptAdd()
+        {
+            _robot.ClickButton("清除");
+            _robot.ClickButton("一");
+            _robot.ClickButton("二");
+            _robot.ClickButton("三");
+            _robot.ClickButton("加");
+            _robot.ClickButton("三");
+            _robot.ClickButton("二");
+            _robot.ClickButton("一");
+            _robot.ClickButton("等於");
+        }
+    }
+}
