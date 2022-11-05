733d66c24c89eef5e73b738468fe134ca85152df
yuanhao <t109590041@ntut.org.tw>
2022-11-05 16:11:44 +0800

    [HomeWork4] project: 加入單元測試專案

---
+++ Homework_4/LibraryManagementSystemTests/Model/BookItemTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Tests
+{
+    [TestClass()]
+    public class BookItemTests
+    {
+        [TestMethod()]
+        public void BookItemTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BookItemTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CopyTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TakeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TakeTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddQuantityTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IsBookEqualsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetInformationListTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ Homework_4/LibraryManagementSystemTests/Model/BookTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibraryManagementSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Tests
+{
+    [TestClass()]
+    public class BookTests
+    {
+        [TestMethod()]
+        public void BookTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BookTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetInformationListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetFormatInformationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
