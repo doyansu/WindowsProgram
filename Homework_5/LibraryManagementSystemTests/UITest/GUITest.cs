0f748b0c91050c802fc8535f4c9fbf6f84e6f512
yuanhao <t109590041@ntut.org.tw>
2022-11-18 23:33:52 +0800

    [HomeWork5] 將 UItest 移到 LibraryManagementSystemTests 專案

---
+++ Homework_5/LibraryManagementSystemTests/UITest/GUITest.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace LibraryManagementSystem.UITest.Tests
+{
+    [TestClass]
+    public class GUITest
+    {
+        private Robot _robot;
+        private const string APP_NAME =
+       "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App";
+        private const string CALCULATOR_TITLE = "小算盤";
+        private const string EXPECTED_VALUE = "顯示是 444";
+        private const string RESULT_CONTROL_NAME = "CalculatorResults";
+
+        private string targetAppPath;
+        private const string START_UP_FORM = "StartUpForm";
+
+        /// <summary>
+        /// Launches the Calculator
+        /// </summary>
+        // init 
+        [TestInitialize]
+        public void Initialize()
+        {
+            //_robot = new Robot(APP_NAME, CALCULATOR_TITLE);
+
+            var projectName = "LibraryManagementSystem";
+            var appName = projectName + ".exe";
+            var solutionName = "Homework_5";
+            string solutionPath = AppDomain.CurrentDomain.BaseDirectory;
+            while (Directory.GetParent(solutionPath).Name != solutionName)
+                solutionPath = Path.GetFullPath(Path.Combine(solutionPath, "..\\"));
+            targetAppPath = Path.Combine(solutionPath, projectName, "bin", "Debug", appName);
+            _robot = new Robot(targetAppPath, START_UP_FORM);
+        }
+        /// <summary>
+        /// Closes the launched program
+        /// </summary>
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _robot.CleanUp();
+        }
+
+        /// <summary>
+        /// Tests that the result of 123 + 321 should be 444
+        /// </summary>
+        [TestMethod]
+        public void TestAdd()
+        {
+            /*RunScriptAdd();
+            _robot.AssertText(RESULT_CONTROL_NAME, EXPECTED_VALUE);*/
+            //_robot.Sleep(1);
+            //_robot.ClickButton("Exit");
+        }
+
+        /// <summary>
+        /// Runs the script: 123 + 321 =
+        /// </summary>
+        private void RunScriptAdd()
+        {
+            _robot.ClickButton("清除");
+            _robot.ClickButton("一");
+            _robot.ClickButton("二");
+            _robot.ClickButton("三");
+            _robot.ClickButton("加");
+            _robot.ClickButton("三");
+            _robot.ClickButton("二");
+            _robot.ClickButton("一");
+            _robot.ClickButton("等於");
+        }
+    }
+}
