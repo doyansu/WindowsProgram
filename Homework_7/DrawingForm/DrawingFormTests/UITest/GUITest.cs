7e581dd7e5f87d7aa201bad98742ed4b2846f43b
yuanhao <t109590041@ntut.org.tw>
2022-12-21 17:16:17 +0800

    [HomeWork7] form GUI test 初始化

---
+++ Homework_7/DrawingForm/DrawingFormTests/UITest/GUITest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace DrawingFormTests.UITest
+{
+    [TestClass]
+    public class GUITest
+    {
+        private Robot _robot;
+        private string DRAWING_FORM = "DrawingForm";
+
+        // 取得目標應用路徑
+        private string GetTargetPath(string projectName)
+        {
+            string appName = projectName + ".exe";
+            string solutionPath = AppDomain.CurrentDomain.BaseDirectory;
+            do
+            {
+                bool IsFindProject = false;
+                foreach (var directory in Directory.GetParent(solutionPath).GetDirectories())
+                    if (directory.Name == projectName)
+                        IsFindProject = true;
+                if (IsFindProject)
+                    break;
+                solutionPath = Path.GetFullPath(Path.Combine(solutionPath, "..\\"));
+            } while (solutionPath != Directory.GetDirectoryRoot(AppDomain.CurrentDomain.BaseDirectory));
+            return Path.Combine(solutionPath, projectName, "bin", "Debug", appName);
+        }
+
+        // init 
+        [TestInitialize]
+        public void Initialize()
+        {
+            const string projectName = "DrawingForm";
+            string targetAppPath;
+            // 只是為了 coverage (projectName 錯誤情況)
+            targetAppPath = this.GetTargetPath("");
+            targetAppPath = this.GetTargetPath(projectName);
+            _robot = new Robot(targetAppPath, DRAWING_FORM);
+        }
+
+        /// <summary>
+        /// Closes the launched program
+        /// </summary>
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            _robot.CleanUp();
+        }
+
+        /// <summary>
+        /// 測試 Borrowing Form 初始化狀態
+        /// </summary>
+        [TestMethod]
+        public void TestFormInitialize()
+        {
+            
+        }
+    }
+}
